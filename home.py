cdd0f6315b520f0849ac49093f94ceec062d82f6
DonZa47 <nudbun73@gmail.com>
2025-03-11 11:33:40 +0700

    Update home.py

--- home.py
+++ home.py
@@ -13,4 +13,17 @@ c1,c2,c3=st.columns(3)
 with c1:
     st.wrine('./img/H1.jpg')
 with c2:
-    st.wrine('.')
+    st.wrine('./img/H2.Jpg')
+with c3:
+    st.write("")
+
+dt= pd.read_csv('./data/heart 2.csv')
+
+st.header("ข้อมูลโรคหัวใจ")
+st.write(dt.head(10))
+
+count_male = dt.groupby('Sex').size()[1]
+count_female = dt.groupby('Sex').size()[0]
+dx = [count_male, count_female]
+dx2 = pd.DataFrame(dx, index=["Male", "Female"])
+st.bar_chart(dx2)
